c59018e18ea72604b0d68b8bc096b79a15861825
Viktor Mandlbauer <viktor.mandlbauer@gmail.com>
2024-07-21 22:09:08 +0200

    Database container for todo app

---
+++ exercises/Project 1.02/ToDo/db/init.sql
@@ -0,0 +1,15 @@
+-- Create simple database for the todo app
+CREATE DATABASE todo_app;
+
+--- Use the database
+\c todo_app
+
+--- Create the task_status enum
+CREATE TYPE task_status AS ENUM ('not-done', 'done');
+
+--- Create the tasks table
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    status task_status NOT NULL DEFAULT 'not-done'
+);
